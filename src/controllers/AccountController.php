291e8e01ec5a870df178a2eca423bfa9755085a3
E.Alamo <erosdelalamo@gmail.com>
2024-04-24 15:04:21 +0200

    Update calls to classname and fix messed code

--- src/controllers/AccountController.php
+++ src/controllers/AccountController.php
@@ -25,7 +25,7 @@ class AccountController extends Controller
     {
         return [
             'verbs' => [
-                'class' => VerbFilter::className(),
+                'class' => VerbFilter::class,
                 'actions' => [
                     'delete' => ['POST'],
                 ],
